1cd5e4e0e5e583233f8a13b70a5edb69c5c4d0b5
Ronald Mosomi <ronaldmosomi@gmail.com>
2021-09-24 16:54:12 +0300

    hypothesis testing ip

--- functions.py
+++ functions.py
@@ -1,1 +1,8 @@
-print('1==2')
+import numpy as np
+def systematic_sampling(df,r,n):
+    step=df.shape[0]//n
+    indexes = np.arange(start=r, stop= len(df), step=step)
+    systematic_sample = df[indexes]
+    return systematic_sample#, indexes
+    
+
